cf1ba497c29998966f6a2f3ecc3066c8ed03e7c1
Kyle Lemons <kyle@kylelemons.net>
2011-04-16 17:29:51 -0700

    Added a skeleton run() to core

--- src/pkg/ircd/core.go
+++ src/pkg/ircd/core.go
@@ -1,16 +1,24 @@
 package ircd
 
 import (
+	"fmt"
+	"kevlar/ircd/conn"
+	"kevlar/ircd/parser"
 	ds "kevlar/ircd/datastore"
 )
 
 type Core struct {
 	Data *ds.DataStore
+	ports map[int]bool
+	listener *conn.Listener
+	messages chan *parser.Message
 }
 
 func NewCore() *Core {
 	core := &Core{
 		Data: ds.NewDataStore(),
+		ports: make(map[int]bool),
+		messages: make(chan *parser.Message),
 	}
 	return core
 }
@@ -42,4 +50,32 @@ func (c *Core) Get(module, key string, def ds.Value) ds.Value {
 
 func (c *Core) Start() {
 	//TODO(kevlar)
+
+	// Listen on each port
+	c.listener = conn.NewListener()
+	for _,port := range c.Get("Server", "ports", []int{6666,6667}).([]int) {
+		c.listener.AddPort(port)
+		c.ports[port] = true
+	}
+	go func() {
+		for conn := range c.listener.Incoming {
+			conn.Subscribe(c.messages)
+		}
+	}()
+
+	go c.run()
+}
+
+func (c *Core) Stop() {
+	// Listen on each port
+	for port := range c.ports {
+		c.listener.ClosePort(port)
+		c.ports[port] = false,false
+	}
+}
+
+func (c *Core) run() {
+	for message := range c.messages {
+		fmt.Printf("Message: %s", message)
+	}
 }
