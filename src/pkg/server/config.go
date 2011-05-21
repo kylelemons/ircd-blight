e069f302714a265e253ec9e13a38f952c1d126b9
Kyle Lemons <kyle@kylelemons.net>
2011-05-21 01:10:11 -0700

    Added port ranges and tests

--- src/pkg/server/config.go
+++ src/pkg/server/config.go
@@ -4,14 +4,18 @@ import (
 	"bytes"
 	"io/ioutil"
 	"os"
+	"strconv"
+	"strings"
 	"xml"
 )
 
+// a Password stores Passwords for Oper and User directives.
 type Password struct {
 	Type     string "attr"
 	Password string "chardata"
 }
 
+// An Oper is an operator configuration directive.
 type Oper struct {
 	Name     string "attr"
 	Password *Password
@@ -19,24 +23,80 @@ type Oper struct {
 	Flag     []string
 }
 
+// A Class is a user/server connection class directive.
 type Class struct {
 	Name string "attr"
 	Host []string
 	Flag []string
 }
 
+// A Link represents the configuration information for a remote
+// server link.
 type Link struct {
 	Name string "attr"
 	Host []string
 	Flag []string
 }
 
+// A Ports direcive stores a port range and whether or not it is an SSL port.
+type Ports struct {
+	SSL        string "attr"
+	PortString string "chardata"
+}
+
+// GetPortList gets the port list specified by the range(s) in this ports directive.
+// The following port range formats are understood:
+//   6667           // A single port
+//   6666-6669      // A port range
+//   6666-6669,6697 // Comma-separated ranges
+func (p *Ports) GetPortList() (ports []int, err os.Error) {
+	ranges := strings.Split(p.PortString, ",", -1)
+	for _, rng := range ranges {
+		extremes := strings.Split(strings.TrimSpace(rng), "-", -1)
+		if len(extremes) > 2 {
+			return nil, os.NewError("Invalid port range: " + rng)
+		}
+		low, err := strconv.Atoi(extremes[0])
+		if err != nil {
+			return nil, err
+		}
+		if len(extremes) == 1 {
+			ports = append(ports, low)
+			continue
+		}
+		high, err := strconv.Atoi(extremes[1])
+		if err != nil {
+			return nil, err
+		}
+		if low > high {
+			return nil, os.NewError("Inverted range: " + rng)
+		}
+		for port := low; port <= high; port++ {
+			ports = append(ports, port)
+		}
+	}
+	return
+}
+
+// AreSSL returns true if the ports represented by the Ports directive
+// are intended to be SSL-enabled ports.
+func (p *Ports) AreSSL() bool {
+	switch strings.ToLower(p.SSL) {
+	case "1", "true", "yes", "on", "ssl", "enabled":
+		return true
+	}
+	return false
+}
+
+// A Network represents the configuration data for the network on which
+// this server is running.
 type Network struct {
 	Name        string "attr"
 	Description string
 	Link        []*Link
 }
 
+// A Configuration stores the configuration information for this server.
 type Configuration struct {
 	Name     string "attr"
 	Admin    string
@@ -46,8 +106,12 @@ type Configuration struct {
 	Operator []*Oper
 }
 
+// A suitable default XML configuration file on which an admin should
+// base his config.xml.
 var DefaultXML = `
 <server name="blight.local">
+	<ports>6666-6669</ports>
+	<ports ssl="true">6696-6699,9999</ports>
 	<network name="IRCD-Blight">
 		<description>An unconfigured IRC network.</description>
 		<link name="blight2.local">
@@ -73,6 +137,8 @@ var DefaultXML = `
 
 var Config *Configuration
 
+// LoadConfigFile loads an XML configuration string of the format shown in DefaultXML
+// as the configuration for the server.
 func LoadConfigString(confxml string) os.Error {
 	conf, err := parseXMLConfig([]byte(confxml))
 	if err != nil {
@@ -82,6 +148,8 @@ func LoadConfigString(confxml string) os.Error {
 	return nil
 }
 
+// LoadConfigFile loads an XML configuration file of the format shown in DefaultXML
+// as the configuration for the server.
 func LoadConfigFile(filename string) os.Error {
 	confxml, err := ioutil.ReadFile(filename)
 	if err != nil {
--- src/pkg/server/config_test.go
+++ src/pkg/server/config_test.go
@@ -57,3 +57,43 @@ func TestDefaultConfig(t *testing.T) {
 		t.Errorf("config = %#v, got %#v", got, want)
 	}
 }
+
+var portRangeTest = []struct {
+	Range       string
+	ExpectPorts []int
+}{
+	{
+		Range:       "6667",
+		ExpectPorts: []int{6667},
+	},
+	{
+		Range:       "6666-6669",
+		ExpectPorts: []int{6666, 6667, 6668, 6669},
+	},
+	{
+		Range:       "6666-6669,6697",
+		ExpectPorts: []int{6666, 6667, 6668, 6669, 6697},
+	},
+}
+
+func TestPortRanges(t *testing.T) {
+	for idx, test := range portRangeTest {
+		directive := &Ports{
+			PortString: test.Range,
+		}
+		ports, err := directive.GetPortList()
+		if err != nil {
+			t.Errorf("#%d: Error: %s", idx, err)
+			continue
+		}
+		if got, want := len(ports), len(test.ExpectPorts); got != want {
+			t.Errorf("#%d: len(ports) = %d, want %d", idx, got, want)
+			continue
+		}
+		for i := 0; i < len(ports); i++ {
+			if got, want := ports[i], test.ExpectPorts[i]; got != want {
+				t.Errorf("#%d: ports[%d] = %d, want %d", idx, i, got, want)
+			}
+		}
+	}
+}
